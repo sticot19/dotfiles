4bc38ce121cdb5485c8ed0fb550a409a07e261ee
sticot19 <sticot19.dums@gmail.com>
2025-11-06 11:17:34 +0100

    base colors

--- swww/swww-carroussel.sh
+++ swww/swww-carroussel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WALLPAPERS_DIR="$HOME/Pictures/wallpapers/"
+WALLPAPERS_DIR="$HOME/Pictures/Wallpapers2025/"
 
 # Lecture et sélection aléatoire en une étape
 wallpaper=$(find "$WALLPAPERS_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -print0 | shuf -zn1 | tr -d '\0')
