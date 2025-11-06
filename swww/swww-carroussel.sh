89e0aeabeb2a3b46d53566fbce56cf563a548877
sticot19 <sticot19.dums@gmail.com>
2025-11-06 10:46:01 +0100

    correction script caroussel

--- swww/swww-carroussel.sh
+++ swww/swww-carroussel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WALLPAPERS_DIR="$HOME/Pictures/wallpapers"
+WALLPAPERS_DIR="$HOME/Pictures/wallpapers/"
 
 # Lecture et sélection aléatoire en une étape
 wallpaper=$(find "$WALLPAPERS_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -print0 | shuf -zn1 | tr -d '\0')
@@ -9,4 +9,4 @@ wallpaper=$(find "$WALLPAPERS_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" 
 swww img "$wallpaper" --transition-type any --transition-fps 60 --transition-duration 2
 
 # Application des couleurs avec matugen
-matugen image "$wallpaper" --apply
+matugen image "$wallpaper"
